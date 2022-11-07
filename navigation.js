7b4609d041fe3b900bd7721ef68bbb1a24fb0178
[200~godspowerufotgit config --global user.name godspowerufotgit config --global user.name [200~godspowerufot <ufotgodspower8@gmail.com>
2022-11-07 06:28:25 -0800

    new update

---
+++ navigation.js
@@ -0,0 +1,12 @@
+function closeNav() {
+    document.querySelector(".sideNav").style.width = "0%";
+
+
+}
+function openNav() {
+    document.querySelector(".sideNav").style.width = "70%";
+    document.querySelector(".sideNav").style.display = "block";
+
+
+
+}
